033d850303fdbec37d0d1fcfe89fe4251da30776
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-06-21 23:42:01 +0530

    chores: added user profile component ✅

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,24 +1,29 @@
-"use client"
+"use client";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import UserProfile from "@/components/todovex/user-profile";
 
 function page() {
-    const tasks = useQuery(api.tasks.get);
+  const tasks = useQuery(api.tasks.get);
 
-    return (
-        <main className='w-full'>
-            <div className="text-center mt-10">
-                <h1 className="text-3xl font-bold underline">
-                    Welcome to the logged in page!
-                </h1>
-            </div>
+  return (
+    <main className="w-full">
+      <div className="text-center mt-10 flex justify-center items-center gap-2">
+        <UserProfile />
+        <h1 className="text-3xl font-bold underline">
+          Welcome to the logged in page!
+        </h1>
+      </div>
 
-            <ul className="w-[3/2] border p-5 list-disc list-outside ">
-                {tasks?.map(({ _id, text }) => <li className="text-xl pl-2" key={_id}>{text}</li>)}
-
-            </ul>
-        </main>
-    )
+      <ul className="w-[3/2] border p-5 list-disc list-outside ">
+        {tasks?.map(({ _id, text }) => (
+          <li className="text-xl pl-2" key={_id}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
-export default page
+export default page;
