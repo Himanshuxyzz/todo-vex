a8e8c76a54ea614be3e36b1fa60b7ff7b307b6f3
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-06-21 17:37:28 +0530

    chores: added sample data and accessed it in ui ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
-import { Button } from "@/components/ui/button"
+"use client";
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+import { Button } from "@/components/ui/button";
+
+
+
 
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
+
   return (
     <main className="h-screen w-full flex flex-col gap-2.5 justify-center items-center">
       <h1 className="font-bold text-center text-2xl ">
@@ -8,6 +16,10 @@ export default function Home() {
       </h1>
       <Button>Click me!
       </Button>
+      <ul className="w-[3/2] border p-5 list-disc list-outside ">
+        {tasks?.map(({ _id, text }) => <li className="text-xl pl-2" key={_id}>{text}</li>)}
+
+      </ul>
     </main>
   );
 }
