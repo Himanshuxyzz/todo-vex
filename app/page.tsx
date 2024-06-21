3aba2392790399d12f457ed30cce6f9cfe55d72d
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-06-21 16:36:28 +0530

    chores: added some styling in the boiler plate ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main >
-      <h1 className="font-bold">
+    <main className="h-screen w-full flex justify-center items-center">
+      <h1 className="font-bold text-center text-2xl ">
         next js boiler plate ✨
       </h1>
     </main>
