c5dc60ac80a6056715c06dd8b2f50481e1e55a52
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-06-21 17:15:26 +0530

    feat: added convex and imported button component from shadcn ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <main className="h-screen w-full flex justify-center items-center">
+    <main className="h-screen w-full flex flex-col gap-2.5 justify-center items-center">
       <h1 className="font-bold text-center text-2xl ">
         next js boiler plate ✨
       </h1>
+      <Button>Click me!
+      </Button>
     </main>
   );
 }
